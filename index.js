d84e57fe3e100ae6f9fdb9252712727e2c329162
Yago <almeidayago167@gmail.com>
2024-04-24 12:52:11 -0400

    complete array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // code your solution here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(record) {
+//   let superbowlYear = record.find(function(obj) {
+    // if(obj["result"] === "W") {
+    //   let winningYear = obj["year"]
+    //   return winningYear
+    // }
+//     return obj["result"] === "W"
+//   })
+//   if(superbowlYear === undefined) {
+//     return undefined
+//   }
+//   else if(superbowlYear !== undefined) {
+//     return superbowlYear["year"]
+//   }
+// }
+// const array1 = [ers, erf, erg]
+// function abc(array) {
+//   return array[0] === true
+// }
+// const foundItem = array1.find(abc) 
